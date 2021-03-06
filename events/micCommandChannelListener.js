d7fad874a356ef9e98044b94951111a6df3f9b33
Devon S <20716219+NintenZone@users.noreply.github.com>
2021-03-06 12:33:10 -0700

    Mic Event Fix (#1)
    Mic event will now detect channel change events properly.

--- events/micCommandChannelListener.js
+++ events/micCommandChannelListener.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 
 module.exports = (bot, oldState, newState) => {
     let data = JSON.parse(fs.readFileSync("./data/mic.json"));
-    
+
     if (newState.channel) {
         if (data["channels"] && data["channels"][newState.channel.id]) {
             if (data["casters"] && data["casters"][newState.member.user.id]) {
                 if (!data[data["channels"][newState.channel.id]]) data[data["channels"][newState.channel.id]] = {};
                 data[data["channels"][newState.channel.id]][newState.member.user.id] = data["casters"][newState.member.user.id];
+            }
+        }
+        else if (oldState.channel && data["channels"] && data["channels"][oldState.channel.id]) {
+            if (data["casters"] && data["casters"][oldState.member.user.id]) {
+                delete data[data["channels"][oldState.channel.id]][oldState.member.user.id];
             }
         }
     }
